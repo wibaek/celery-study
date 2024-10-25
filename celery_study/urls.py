44b747d74425fddb03965a3014e3ceb166265412
Wibaek Park <wibaek@ninewatt.com>
2024-10-25 14:49:43 +0900

    rename: 장고 프로젝트명 celery_study로 변경

--- celery_study/urls.py
+++ celery_study/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for learn_celery project.
+URL configuration for celery_study project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- celery_study/wsgi.py
+++ celery_study/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for learn_celery project.
+WSGI config for celery_study project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learn_celery.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "celery_study.settings")
 
 application = get_wsgi_application()
